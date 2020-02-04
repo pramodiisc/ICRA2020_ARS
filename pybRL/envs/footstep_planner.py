95df91959ce3a31b66e6c208aafefd54221082db
sashank tirumala <sashank.tirumala@nymble.in>
2020-02-04 18:00:18 +0530

    Added a function to calculate the velocity of each foot given a command, in that foots coordinate frame, tested seems to work, currently put some arbitrary limit for maximum velocity, which prolly needs to change

--- pybRL/envs/footstep_planner.py
+++ pybRL/envs/footstep_planner.py
@@ -1,8 +1,27 @@
 """Implementation of different footstep planners, for now only 1 footstep planner is implemented """
 import numpy as np
-# from cvxopt import matrix, solvers
+import sys, os
+sys.path.append(os.path.realpath('../..'))
+
+from cvxopt import matrix, solvers
 import cvxopt
 import time
+import pybRL.utils.frames as frames
+# Robot constants
+bodyWidth = 0.24
+bodyLength = 0.37
+#En
+tf = frames.TransformManager()
+t_FL = np.array([[1,0,0,bodyLength/2],[0,1,0,0],[0,0,-1,-bodyWidth/2],[0,0,0,1]])
+t_FR = np.array([[1,0,0,bodyLength/2],[0,1,0,0],[0,0, 1,bodyWidth/2],[0,0,0,1]])
+t_BR = np.array([[1,0,0,-bodyLength/2],[0,1,0,0],[0,0,1,bodyWidth/2],[0,0,0,1]])
+t_BL = np.array([[1,0,0,-bodyLength/2],[0,1,0,0],[0,0,-1,-bodyWidth/2],[0,0,0,1]])
+ 
+tf.add_transform('FL', 'COM', t_FL)
+tf.add_transform('FR', 'COM', t_FR)
+tf.add_transform('BR', 'COM', t_BR)
+tf.add_transform('BL', 'COM', t_BL)
+
 
 def linear_program_ver1(prev_footstep, command):
     """ The optimization problem was first tested in Mathematica in opt_test.nb, Then the
@@ -35,12 +54,28 @@ def linear_program_ver1(prev_footstep, command):
     print(sol['x'])
     return sol['x']
 
-
+def get_v_in_footstep_coords(command, foot_name, prev_footstep):
+    """
+    Very important to normalize V in the current optimization framework
+    prev_footstep is a numpy array of values x,y,z
+    foot_name is a string 'FR', 'FL', 'BR', 'BL'
+    """
+    v_norm_max = 0.3808 #Max possible end effector velocity, need to calculate, probably changes at every position, need to use Jacobian I guess
+    v = command[0]
+    omega = command[1]
+    r = frames.transform_points(tf.get_transform(foot_name, 'COM'), prev_footstep)
+    v_final = -1*frames.Normalize(v + frames.Cross(omega, r), v_norm_max)
+    v_final = tf.get_transform('COM', foot_name)[:3, :3]@v_final
+    return v_final
 if(__name__ == "__main__"):
     #Testing ver1 code
     #Right now it seems like it takes 5 Micro Seconds, which is very good, as long as it's less than 0.1ms its fine
     # print(time.time())
-    start_time = time.time()
-    linear_program_ver1(np.array([0.068,0,0]), np.array([0.3808,0,0]))
-    time_elapsed = time.time() - start_time
+    # start_time = time.time()
+    # linear_program_ver1(np.array([0.068,0,0]), np.array([0.3808,0,0]))
+    # time_elapsed = time.time() - start_time
     # print(time_elapsed)
+    #There is problem with the optimization, gives me infeasible conditions when Mathematica finds feasible solutions
+    #tof of 0 leads to infinite frequency, How to handle 
+    command = [np.array([0,0,0]), np.array([0,1,0])]
+    print(get_v_in_footstep_coords(command,'BR',np.array([0,0,0])))
