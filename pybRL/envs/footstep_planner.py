4f4c91139209f0e4345c3f222da65794d457f38b
sashank tirumala <sashank.tirumala@nymble.in>
2020-02-05 10:50:09 +0530

    Added code to find intersection line and an ellipse analytically, tested works fine

--- pybRL/envs/footstep_planner.py
+++ pybRL/envs/footstep_planner.py
@@ -10,6 +10,8 @@ import pybRL.utils.frames as frames
 # Robot constants
 bodyWidth = 0.24
 bodyLength = 0.37
+stepLengthx = 0.136
+stepLengthz = 0.017
 #En
 tf = frames.TransformManager()
 t_FL = np.array([[1,0,0,bodyLength/2],[0,1,0,0],[0,0,-1,-bodyWidth/2],[0,0,0,1]])
@@ -35,7 +37,7 @@ def linear_program_ver1(prev_footstep, command):
     """
     #Solver options for max speed
     cvxopt.solvers.options['glpk'] = {'msg_lev': 'GLP_MSG_OFF'}
-    cvxopt.solvers.options['show_progress'] = True
+    cvxopt.solvers.options['show_progress'] = False
     #Auxiliary functions taken from Mathematica
     minToF = 1/2.8
     maxToF = 1
@@ -60,6 +62,7 @@ def get_v_in_footstep_coords(command, foot_name, prev_footstep):
     prev_footstep is a numpy array of values x,y,z
     foot_name is a string 'FR', 'FL', 'BR', 'BL'
     """
+    #This equation can possibly work in 3D
     v_norm_max = 0.3808 #Max possible end effector velocity, need to calculate, probably changes at every position, need to use Jacobian I guess
     v = command[0]
     omega = command[1]
@@ -67,6 +70,21 @@ def get_v_in_footstep_coords(command, foot_name, prev_footstep):
     v_final = -1*frames.Normalize(v + frames.Cross(omega, r), v_norm_max)
     v_final = tf.get_transform('COM', foot_name)[:3, :3]@v_final
     return v_final
+
+def calculate_step_length(footstep, velocity):
+    #Currently only a 2D Equation
+    px, py, pz = footstep
+    vx, vy, vz = velocity
+    #Below equation is taken from Mathematica
+    t_1 =((-4*px*vx)/stepLengthx**2 - (4*pz*vz)/stepLengthz**2 \
+        - (4*np.sqrt(-(pz**2*vx**2) + (stepLengthz**2*vx**2)/4.\
+         + 2*px*pz*vx*vz - px**2*vz**2 + (stepLengthx**2*vz**2)/4.))\
+            /(stepLengthx*stepLengthz))/-((4*vx**2)/stepLengthx**2 + (4*vz**2)/stepLengthz**2)
+    t_2 =((-4*px*vx)/stepLengthx**2 - (4*pz*vz)/stepLengthz**2 \
+        + (4*np.sqrt(-(pz**2*vx**2) + (stepLengthz**2*vx**2)/4.\
+         + 2*px*pz*vx*vz - px**2*vz**2 + (stepLengthx**2*vz**2)/4.))\
+        /(stepLengthx*stepLengthz))/-((4*vx**2)/stepLengthx**2 + (4*vz**2)/stepLengthz**2)
+    return t_1,t_2
 if(__name__ == "__main__"):
     #Testing ver1 code
     #Right now it seems like it takes 5 Micro Seconds, which is very good, as long as it's less than 0.1ms its fine
@@ -77,5 +95,11 @@ if(__name__ == "__main__"):
     # print(time_elapsed)
     #There is problem with the optimization, gives me infeasible conditions when Mathematica finds feasible solutions
     #tof of 0 leads to infinite frequency, How to handle 
-    command = [np.array([0,0,0]), np.array([0,1,0])]
-    print(get_v_in_footstep_coords(command,'BR',np.array([0,0,0])))
+    footstep = np.array([0,0,0])
+    command = [np.array([1,0,0]), np.array([0,0,0])]
+    vf = get_v_in_footstep_coords(command,'FL',footstep)
+    # vf = frames.Normalize(vf)
+    print("vf: ",vf)
+    sl = calculate_step_length(footstep, vf)
+    print(sl)
+    print(footstep+sl[0]*vf, footstep+sl[1]*vf)
